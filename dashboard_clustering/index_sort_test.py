9106c652939ffca655d0f21e8c727d3e3f17c825
Marina-Andric <marina.andric@imtlucca.it>
2018-10-01 20:19:51 +0200

    deleted redundant dirs

--- dashboard_clustering/index_sort_test.py
+++
@@ -1,5 +0,0 @@
-s = [3, 4, 2, 3, 0]
-
-res = sorted(range(len(s)), key = lambda k: s[k])
-
-print (res)
--- forecasting/neural_networks.py
+++
@@ -1,4 +0,0 @@
-import numpy as np
-import pandas as pd
-
-# svm and artificial neural networks
